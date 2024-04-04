8b56696d0a71c0c5265190940b07de983ca4b4ad
Patrizio Bellan <patrizio.bellan@gmail.com>
2024-04-04 20:34:30 +0200

    add setup.py

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(
+        name='PETVisualizer',
+        version='1.1.1',
+        packages=[],
+        url='',
+        license='MIT',
+        author='xxxx',
+        author_email='',
+        description='PET Visualizer of the PET Dataset'
+)
